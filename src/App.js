1e55fa80d3bf5738a18604f2f4cb143434683b3e
Nicholas Mendez <mendez.nicholasr@gmail.com>
2022-06-24 16:31:16 -0400

    removed startup code , created Navbar and Setup Links

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import {React , Fragment} from 'react'
+import {Routes , Route} from 'react-router-dom' ;
+
+import Navigation from './Navigation';
+import HomePage from './home/HomePage'
+import Clients from './clients/Clients';
+import Schedule from './schedule/Schedule';
+import Payroll from './payroll/Payroll';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Fragment>
+      <Navigation/>
+        <Routes>
+            <Route path="clients" element={<Clients />} />
+            <Route path="schedule" element={<Schedule />} />
+            <Route path="payroll" element={<Payroll />} />
+            <Route path="/" element={<HomePage />} />
+        </Routes>
+    </Fragment>
+  )
 }
 
 export default App;
---
+++ src/Navigation.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import { Navbar , Nav , Container } from 'react-bootstrap'
+
+
+const Navigation = () => {
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Container>
+       <Navbar.Brand as={Link} to="/">Appointments</Navbar.Brand>
+        <Nav className="justify-content-end">
+          <Nav.Link as={Link} to="/clients" exact>Clients</Nav.Link>
+          <Nav.Link as={Link} to="/schedule" exact>Schedule</Nav.Link>
+          <Nav.Link as={Link} to="/payroll" exact>Payroll</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  )
+}
+
+export default Navigation
